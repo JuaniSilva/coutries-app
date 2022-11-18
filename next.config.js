8e45e2255f1fa2dcf6e017c1d99a9ddfd13bbcc2
JuaniSilva <juanicruzsilva@gmail.com>
2022-11-17 21:19:22 -0300

    feat: project config update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	experimental: {
+		appDir: true
+	},
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "**",
+			},
+		],
+	},
 }
 
 module.exports = nextConfig
